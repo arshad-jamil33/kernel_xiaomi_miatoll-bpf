e391d4d2adbcf03017f067bdc294baa3dd41109c
Andrei Vagin <avagin@openvz.org>
2018-04-10 16:30:44 -0700

    procfs: add seq_put_hex_ll to speed up /proc/pid/maps
    seq_put_hex_ll() prints a number in hexadecimal notation and works
    faster than seq_printf().
    
    == test.py
      num = 0
      with open("/proc/1/maps") as f:
              while num < 10000 :
                      data = f.read()
                      f.seek(0, 0)
                     num = num + 1
    ==
    
    == Before patch ==
      $  time python test.py
    
      real	0m1.561s
      user	0m0.257s
      sys	0m1.302s
    
    == After patch ==
      $ time python test.py
    
      real	0m0.986s
      user	0m0.279s
      sys	0m0.707s
    
    $ perf -g record python test.py:
    
    == Before patch ==
    -   67.42%     2.82%  python   [kernel.kallsyms] [k] show_map_vma.isra.22
       - 64.60% show_map_vma.isra.22
          - 44.98% seq_printf
             - seq_vprintf
                - vsnprintf
                   + 14.85% number
                   + 12.22% format_decode
                     5.56% memcpy_erms
          + 15.06% seq_path
          + 4.42% seq_pad
       + 2.45% __GI___libc_read
    
    == After patch ==
    -   47.35%     3.38%  python   [kernel.kallsyms] [k] show_map_vma.isra.23
       - 43.97% show_map_vma.isra.23
          + 20.84% seq_path
          - 15.73% show_vma_header_prefix
               10.55% seq_put_hex_ll
             + 2.65% seq_put_decimal_ull
               0.95% seq_putc
          + 6.96% seq_pad
       + 2.94% __GI___libc_read
    
    [avagin@openvz.org: use unsigned int instead of int where it is suitable]
      Link: http://lkml.kernel.org/r/20180214025619.4005-1-avagin@openvz.org
    [avagin@openvz.org: v2]
      Link: http://lkml.kernel.org/r/20180117082050.25406-1-avagin@openvz.org
    Link: http://lkml.kernel.org/r/20180112185812.7710-1-avagin@openvz.org
    Signed-off-by: Andrei Vagin <avagin@openvz.org>
    Cc: Alexey Dobriyan <adobriyan@gmail.com>
    Cc: KAMEZAWA Hiroyuki <kamezawa.hiroyu@jp.fujitsu.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -442,6 +442,19 @@ static int show_vma_header_prefix(struct seq_file *m, unsigned long start,
 
 	m->count += len;
 	return 0;
+	seq_setwidth(m, 25 + sizeof(void *) * 6 - 1);
+	seq_put_hex_ll(m, NULL, start, 8);
+	seq_put_hex_ll(m, "-", end, 8);
+	seq_putc(m, ' ');
+	seq_putc(m, flags & VM_READ ? 'r' : '-');
+	seq_putc(m, flags & VM_WRITE ? 'w' : '-');
+	seq_putc(m, flags & VM_EXEC ? 'x' : '-');
+	seq_putc(m, flags & VM_MAYSHARE ? 's' : 'p');
+	seq_put_hex_ll(m, " ", pgoff, 8);
+	seq_put_hex_ll(m, " ", MAJOR(dev), 2);
+	seq_put_hex_ll(m, ":", MINOR(dev), 2);
+	seq_put_decimal_ull(m, " ", ino);
+	seq_putc(m, ' ');
 }
 
 static void
