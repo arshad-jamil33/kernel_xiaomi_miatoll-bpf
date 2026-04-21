bc8aa9e785ecbc5324d1081d35a6356b0ddfca31
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2026-04-21 19:57:42 -0400

    task_work.h: Backport enum task_work_notify_mode

--- include/linux/task_work.h
+++ include/linux/task_work.h
@@ -13,6 +13,13 @@ init_task_work(struct callback_head *twork, task_work_func_t func)
 	twork->func = func;
 }
 
+enum task_work_notify_mode {
+	TWA_NONE,
+	TWA_RESUME,
+	TWA_SIGNAL,
+	TWA_SIGNAL_NO_IPI,
+};
+
 int task_work_add(struct task_struct *task, struct callback_head *twork, bool);
 struct callback_head *task_work_cancel(struct task_struct *, task_work_func_t);
 void task_work_run(void);
