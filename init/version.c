aad307a804d4bccc83abc47587b8c38418dde5ea
KimChi <arshadjamil709@gmail.com>
2026-07-14 15:22:48 +0000

    Spoofed version 5.10.362

--- init/version.c
+++ init/version.c
@@ -27,7 +27,7 @@ struct uts_namespace init_uts_ns = {
 	.name = {
 		.sysname	= UTS_SYSNAME,
 		.nodename	= UTS_NODENAME,
-		.release	= "4.14.357-kinesis-omega-bpf",
+		.release	= "5.10.362-BLACKHOLE-Eclipse",
 		.version	= UTS_VERSION,
 		.machine	= UTS_MACHINE,
 		.domainname	= UTS_DOMAINNAME,
@@ -42,7 +42,7 @@ EXPORT_SYMBOL_GPL(init_uts_ns);
 
 /* FIXED STRINGS! Don't touch! */
 const char linux_banner[] =
-	"Linux version " UTS_RELEASE " (" LINUX_COMPILE_BY "@"
+	"Linux version 5.10.362-BLACKHOLE-Eclipse  (" LINUX_COMPILE_BY "@"
 	LINUX_COMPILE_HOST ") (" LINUX_COMPILER ") " UTS_VERSION "\n";
 
 const char linux_proc_banner[] =
